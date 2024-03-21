86b54271ad59093c9c27280354575f9c0500a3c1
linusapelgren <70014475+linusapelgren@users.noreply.github.com>
2024-03-21 19:40:45 +0100

    Programmed multiple new pages and a menu

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the dropdown button
-    var dropdownbtn = document.querySelector('.dropdownbtn');
-    // Get the dropdown content
-    var dropdownContent = document.querySelector('.dropdown-content');
+  // Get the hamburger button
+  var hamburger = document.querySelector('.dropdownbtn');
 
-    // Add an event listener to the dropdown button
-    dropdownbtn.addEventListener('click', function() {
-      // Toggle the 'active' class on the dropdown content when the button is clicked
-      dropdownContent.classList.toggle('active');
-    });
+  // Get the side navigation menu
+  var sidenav = document.getElementById('mySidenav');
+
+  // Toggle the side navigation menu and change the icon on click
+  hamburger.addEventListener('click', function() {
+    var icon = this.children[0];
+    if (sidenav.style.width === '0px') {
+      sidenav.style.width = '33%'; // Open the side navigation menu
+      icon.classList.remove('fa-bars');
+      icon.classList.add('fa-times');
+    } else {
+      sidenav.style.width = '0'; // Close the side navigation menu
+      icon.classList.remove('fa-times');
+      icon.classList.add('fa-bars');
+    }
+  });
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    var header = document.getElementById("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      header.style.display = "none";
+    } else {
+      header.style.display = "block";
+    }
+  }
 });
